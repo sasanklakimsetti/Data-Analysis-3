250c5aee53faf9ef1908594d9545321aa13469e2
Sasank Lakimsetti <saisasank.lakimsetti@gmail.com>
2024-11-09 00:33:10 +0530

    decided to change the dataset for the analysis

---
+++ script.py
@@ -0,0 +1,4 @@
+import os
+
+# Replace <Your Command Here> with your actual command.
+os.system('kaggle datasets download -d nelgiriyewithana/world-stock-prices-daily-updating -p "C:\\lpu\\5th sem\\INT233\\Project" --unzip')
